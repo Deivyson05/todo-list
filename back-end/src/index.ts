0fd16f99bdb29c5a9fff632ebe39cf504271203a
Deivyson Ricardo Silva dos Santos <131066052+Deivyson05@users.noreply.github.com>
2026-04-06 22:54:21 +0000

    fix: corrige package.json

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import { prisma } from "../lib/prisma";
@@ -9,7 +9,17 @@ import routes from "./routes";
 const app: express.Application = express();
 app.use(cors());
 app.use(express.json());
+//app.use(router);
+
+app.use(express.static("src/public"));
 
 app.use("/api", routes);
 
-export default app;
+app.get("/", (req: Request, res: Response) => {
+    res.send("Servidor Aberto!")
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Teste n1 ${port}`);
+});
