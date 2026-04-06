a33953172f9723d584ed10b321580046677af1dd
Deivyson Ricardo Silva dos Santos <131066052+Deivyson05@users.noreply.github.com>
2026-04-06 22:40:53 +0000

    fix: corrigindo index.ts

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -9,13 +9,7 @@ import routes from "./routes";
 const app: express.Application = express();
 app.use(cors());
 app.use(express.json());
-//app.use(router);
-
-app.use(express.static("src/public"));
 
 app.use("/api", routes);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Teste n1 ${port}`);
-});
+export default app;
