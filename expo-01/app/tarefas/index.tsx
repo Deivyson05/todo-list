b71cced5d2fdcae7537feeac3aefec6c7783a1d7
Deivyson05 <deivysonrssantos@outlook.com>
2026-04-19 18:38:43 -0300

    fix show

--- expo-01/app/tarefas/index.tsx
+++ expo-01/app/tarefas/index.tsx
@@ -10,7 +10,7 @@ import {
   View,
 } from "react-native";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { adicionarTarefa, desmarcarTarefa, excluirTarefa, getTarefas } from "@/back4app";
+import { adicionarTarefa, concluirTarefa, excluirTarefa, getTarefas } from "@/back4app";
 import { TrashIcon } from "lucide-react-native";
 
 export default function TarefasPage() {
@@ -33,15 +33,15 @@ export default function TarefasPage() {
 
       <View className="gap-2 w-full">
         {data?.map((t: any) => {
-          if (t.concluida == false) return null;
+          if (t.completed == false) return null;
           return (
             <View
               className="bg-gray-200 w-full rounded-xl flex-row justify-between items-center overflow-hidden"
-              key={t.objectId}
+              key={t.id}
             >
               <TouchableOpacity className="flex-row gap-4  pl-4 flex-1"
                 onPress={() => {
-                  desmarcarTarefa(t.objectId);
+                  concluirTarefa(t.id);
                   queryClient.invalidateQueries({ queryKey: ["tarefas"] });
                 }}
               >
@@ -51,12 +51,12 @@ export default function TarefasPage() {
                 <Text
                   className="line-through text-gray-500"
                 >
-                  {t.descricao}
+                  {t.title}
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity className="bg-red-600 h-14 w-14 items-center justify-center"
                 onPress={() => {
-                  excluirTarefa(t.objectId);
+                  excluirTarefa(t.id);
                   queryClient.invalidateQueries({ queryKey: ["tarefas"] });
                 }}
               >
